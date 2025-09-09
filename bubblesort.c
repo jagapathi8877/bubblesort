1f17ffe06b340a083fe56effd50b0588bd8461bd
jagapathi8877 <jagapathinamala@gmail.com>
2025-09-09 07:01:12 +0530

    Replace code with user exact implementation and add author/date bannerUpdate bubblesort.c

--- bubblesort.c
+++ bubblesort.c
@@ -1,56 +1,39 @@
 /*
  * Bubble Sort Implementation in C
- * 
- * This program implements the bubble sort algorithm to sort an array
- * of integers in ascending order.
- * 
  * Author: jagapathi8877
  * Date: September 9, 2025
  */
-
-#include <stdio.h>
-
-// Function to perform bubble sort
-void bubbleSort(int arr[], int n) {
-    int i, j, temp;
-    
-    // Outer loop for number of passes
-    for (i = 0; i < n - 1; i++) {
-        // Inner loop for comparisons in each pass
-        for (j = 0; j < n - i - 1; j++) {
-            // Compare adjacent elements
-            if (arr[j] > arr[j + 1]) {
-                // Swap if they are in wrong order
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
+#include<stdio.h>
+int i,j;
+void print_array(int arr[],int n){
+	for(i=0;i<n;i++){
+		printf("%d",arr[i]);
+	}
+	printf("\n");
 }
-
-// Function to print the array
-void printArray(int arr[], int size) {
-    int i;
-    for (i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
+void sort(int arr[],int n){
+	for(i=0;i<n;i++){
+		int swap = 0;
+		for(j=0;j<n-1;j++){
+			if(arr[j]>arr[j+1]){
+				int temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+				swap = 1;
+			}
+		}
+		if(swap){
+			printf("PASS :",i+1);
+			print_array(arr,n);
+		}
+	}
 }
-
-int main() {
-    // Sample array to demonstrate sorting
-    int arr[] = {64, 34, 25, 12, 22, 11, 90};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    
-    printf("Original array: ");
-    printArray(arr, n);
-    
-    // Perform bubble sort
-    bubbleSort(arr, n);
-    
-    printf("Sorted array: ");
-    printArray(arr, n);
-    
-    return 0;
+int main(){
+	int n;
+	scanf("%d",&n);
+	int arr[n];
+	for(i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+	sort(arr,n);
 }
